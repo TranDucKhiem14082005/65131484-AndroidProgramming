6fc3df0b5ff001a2a2961a8ca2af40db6f652d27
TranDucKhiem14082005 <khiemtran015@gmail.com>
2026-03-30 00:22:55 +0700

    learn_practice-RecyclerView

---
+++ UsingRecyclerView/app/src/main/java/khiem/it/usingrecyclerview/MainActivity.java
@@ -0,0 +1,56 @@
+package khiem.it.usingrecyclerview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandScape;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        recyclerViewDatas = getDataForRecyclerView();
+        //4 tim dieu khien
+        recyclerViewLandScape = findViewById(R.id.recyclerland);
+//        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+//        recyclerViewLandScape.setLayoutManager(layoutLinear);
+//        RecyclerView.LayoutManager layoutLinearHorizonal = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+//        recyclerViewLandScape.setLayoutManager(layoutLinearHorizonal);
+        RecyclerView.LayoutManager layoutGrid = new GridLayoutManager(this,2);
+        recyclerViewLandScape.setLayoutManager(layoutGrid);
+        landScapeAdapter = new LandScapeAdapter(this,recyclerViewDatas);
+
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    ArrayList<LandScape> getDataForRecyclerView() {
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("anh1","Thap ap phen");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("anh2","Thap Nghien"));
+         dsDuLieu.add(new LandScape("anh3","Thap Cham"));
+        return  dsDuLieu;
+    }
+
+
+}
