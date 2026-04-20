fae71529804bcc9548a85b59f8abe0cdfcac28f9
TranDucKhiem14082005 <khiemtran015@gmail.com>
2026-04-20 08:08:19 +0700

    LAN THU 2

--- TranDucKhiem/app/src/main/java/thigk2/tranduckhiem/ChucNang2Fragment.java
+++ TranDucKhiem/app/src/main/java/thigk2/tranduckhiem/ChucNang2Fragment.java
@@ -7,41 +7,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChucNang2Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class ChucNang2Fragment extends Fragment {
+    ArrayList<String> dsTenBHCMVN;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ChucNang2Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChucNang2Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
+
     public static ChucNang2Fragment newInstance(String param1, String param2) {
         ChucNang2Fragment fragment = new ChucNang2Fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +36,50 @@ public class ChucNang2Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
+    AdapterView.OnItemClickListener BoLangNgheVaXl = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            String tenBH = dsTenBHCMVN.get(position);
+            Toast.makeText(getContext(), "Bạn vừa chọn bài hát thứ " +
+                    String.valueOf(position + 1) + " - Bài hát tó tên là: " + tenBH,Toast.LENGTH_LONG).show();
+
+
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chuc_nang2, container, false);
+        View view = inflater.inflate(R.layout.fragment_chuc_nang2, container, false);
+        dsTenBHCMVN = new ArrayList<>();
+        dsTenBHCMVN.add("Đất nước trọn niềm vui");
+        dsTenBHCMVN.add("Trần Đức Khiêm");
+        dsTenBHCMVN.add("Bác đang cùng chúng cháu hành quân");
+        dsTenBHCMVN.add("Cô gái mở đường");
+        dsTenBHCMVN.add("Chào em cô gái Lam Hồng");
+        dsTenBHCMVN.add("Tấm áo chiến sĩ mẹ vá năm xưa");
+        dsTenBHCMVN.add("Hát mãi khúc quân hành");
+        dsTenBHCMVN.add("Tiểu đoàn 307");
+        dsTenBHCMVN.add("Năm anh em trên một chiếc xe tăng");
+        dsTenBHCMVN.add("Tàu anh qua núi ");
+
+        // 2. Ánh xạ ListView
+        ListView lvTenTinhThanh = view.findViewById(R.id.LV_DSBH);
+
+        // 3. Adapter
+        ArrayAdapter<String> adapterTinhThanh = new ArrayAdapter<>(
+                getContext(),
+                android.R.layout.simple_list_item_1,
+                dsTenBHCMVN
+        );
+
+        lvTenTinhThanh.setAdapter(adapterTinhThanh);
+
+        // 4. Click item
+        lvTenTinhThanh.setOnItemClickListener(BoLangNgheVaXl);
+        return view;
     }
 }
